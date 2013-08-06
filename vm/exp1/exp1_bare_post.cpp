f9b534e383c9497c1dfad87165a47e5f98f65885
Antti Sykäri <antti.sykari@gmail.com>
2013-08-06 05:31:19 +0300

    vm: typedefs, helpers etc.

--- vm/exp1/exp1_bare_post.cpp
+++ vm/exp1/exp1_bare_post.cpp
@@ -5,34 +5,39 @@ const int a_mask = (1 << a_bits) - 1;
 const int b_bits = 12;
 const int b_mask = (1 << b_bits) - 1;
 
-const int op_exit = 0x80;
 const int op_add = 0x00;
 const int op_sub = 0x01;
 const int op_mul = 0x02;
 const int op_div = 0x03;
+const int op_mod = 0x04;
+
+const int op_exit = 0x0f;
+
+void fail(const char* msg) {
+	printf("%s\n", msg);
+	exit(1);
+}
 
 int main() {
 	uint32 pc = 0;
 
 	while (true) {
 		if (pc >= code_size) {
-			printf("invalid pc");
-			return 1;
+			fail("invalid pc");
 		}
 
-		unsigned int instr = code[pc++];
-		unsigned int b = instr & b_mask;
-		unsigned int a = (instr >> b_bits) & b_mask;
-		unsigned int opcode = (instr >> (a_bits + b_bits)) & opcode_mask;
+		uint32 instr = code[pc++];
+		uint32 b = instr & b_mask;
+		uint32 a = (instr >> b_bits) & b_mask;
+		uint32 opcode = (instr >> (a_bits + b_bits)) & opcode_mask;
 
 		printf("instr: %08x, op: %02x, a: %03x, b: %03x\n", instr, opcode, a, b);
 
 		if (a >= data_size) {
-			printf("invalid memory access: %08x\n", a);
-			return 1;
+			fail("invalid memory access (a)");
 		}
 
-		byte* a_mem = &data[a];
+		byte* a_mem = &m[a];
 		byte b_immediate;
 		byte* b_mem;
 		// Immediate?
@@ -41,15 +46,14 @@ int main() {
 			b_mem = &b_immediate;
 		} else {
 			if (b >= data_size) {
-				printf("invalid memory access: %08x\n", b);
-				return 1;
+				fail("invalid memory access (b)");
 			}
-			b_mem = &data[b];
+			b_mem = &m[b];
 		}
 		
 		switch (opcode) {
 			case op_exit:
-				printf("exit with value %d\n", *b_mem);
+				printf("exit value: %d\n", *b_mem);
 				return 0;
 			case op_add:
 				*a_mem += *b_mem;
@@ -63,9 +67,11 @@ int main() {
 			case op_div: 
 				*a_mem /= *b_mem;
 				break;
+			case op_mod: 
+				*a_mem %= *b_mem;
+				break;
 			default:
-				printf("invalid instruction\n");
-				return 1;
+				fail("invalid instruction\n");
 		}
 	}
 }
